c4780e8654b271cea9d885b5763682a4b2594fae
Rafa Lourenço <rafalourenc123641@gmail.com>
2021-03-08 12:57:24 -0300

    ep1 - Criando funções principais para manipular os tipos de structs

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,3 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM_INFO 64
+
+typedef struct node{
+  struct node * proximo;
+  struct node * antescessor;
+  char  info[TAM_INFO];
+}Node;
+
+typedef struct cabecaLista {
+  struct node * iniLista;
+  struct node * fimLista;
+} CabecaLista;
+
+void adicionarNode(CabecaLista* lista, char info[]){
+  if(lista->iniLista == NULL || lista->fimLista == NULL){
+    Node* node;
+    node = malloc(sizeof(node));
+    node->proximo = NULL;
+    node->antescessor = NULL;
+    strcpy(node->info, info);
+    lista->iniLista = (Node*) node;
+  }
+}
+
+void printaLista(CabecaLista lista, Node* nodeAtual){
+  //while(nodeAtual->proximo != NULL){
+    printf("%x", (Node*) lista->iniLista);
+    printf("%c", (char) nodeAtual->info);
+    //return printaLista(lista, nodeAtual->proximo);
+  //}
+    //printf("%s", nodeAtual->info);
+    return;
+}
+
 int main (void){
-    return 0;
+  struct cabecaLista lista;
+  char info[TAM_INFO] = {'R', 'A'};
+  adicionarNode(&lista, info);
+
+  printaLista(lista, lista.iniLista);
+
+  return 0;
 }
