7dce3da9beafe6b5fd569c4a7ca0daa82a0a3e87
Rafa Lourenço <rafalourenc123641@gmail.com>
2021-03-11 19:06:11 -0300

    Sistema principal completo

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+/* 
+ ** Constantes
+**/
+
+/* Tamanho da array instrucoes e nomes 
+ * eh preciso tratar ela e separar em duas strings
+**/
+#define TAM_INSTRUCAO_E_NOME 100 
+
+/* Tamanho da string de nome exigida no enunciado
+**/
 #define TAM_INFO 64
+
+/*
+ ** Tipos criados
+**/
 
 typedef struct node{
   struct node * proximo;
@@ -12,14 +28,23 @@ typedef struct node{
 
 typedef Node* ponteiroNode;
 
+
 typedef struct cabecaLista {
   struct node * iniLista;
   struct node * fimLista;
 } CabecaLista;
 
-CabecaLista iniciaLista(){
-  CabecaLista lista;
-  lista.iniLista = lista.fimLista = NULL;
+typedef CabecaLista* Lista;
+
+
+/*
+ ** Funcoes de manipulacao da lista
+**/
+
+Lista iniciaLista(){
+  Lista lista;
+  lista = malloc(sizeof (CabecaLista));
+  lista->iniLista = lista->fimLista = NULL;
   return lista;
 }
 
@@ -32,67 +57,111 @@ ponteiroNode criaNode(char info[TAM_INFO]){
   return node;
 }
 
-CabecaLista adicionarNode(CabecaLista lista, char info[TAM_INFO]){
+Lista adicionarNome(Lista lista, char info[TAM_INFO]){
   ponteiroNode node = criaNode(info);
 
-  if(lista.iniLista == NULL) lista.fimLista = NULL;
-  else node->proximo = lista.iniLista;
+  if(lista->iniLista == NULL) lista->fimLista = NULL;
+  else node->proximo = lista->iniLista;
   
-  lista.iniLista = node;
+  lista->iniLista = node;
 
   return lista;
 }
 
 void printaLista(ponteiroNode nodeAtual){  
   if(nodeAtual == NULL) return;
-  printf("%s ", nodeAtual->info);
-  printf("\n");
+  printf("%s", nodeAtual->info);
   printaLista(nodeAtual->proximo);
 }
 
-void liberaLista(ponteiroNode nodeAtual) {
+
+/*
+ ** Funcoes para liberar a lista
+**/
+
+void liberaNos (ponteiroNode nodeAtual) {
   if (nodeAtual == NULL) return;
-  liberaLista(nodeAtual->proximo);  // libera o restante
+  liberaNos(nodeAtual->proximo);
   free(nodeAtual); 
 }
 
+void liberaLista(Lista lista){
+  if(lista->iniLista != NULL) liberaNos(lista->iniLista);
+  free(lista);
+}
+
+/*
+ ** Funcoes do menu
+**/
+
+int tratarInstrucaoENome(char str[], Lista lista){
+  int i;  
+  for(i=0; !isspace(str[i]) ;i++);
+  //quando sai dessa estrutura de repeticao significa que achamos o primeiro espaco
+
+  //cria um ponteiro de char para receber a substring apos o primeiro espaco
+  char* nome;
+  nome = &str[i+1]; //recebe substring com a info(nome) para alocar na lista 
+
+  /*o indice zero da str recebida guarda o dado necessario para saber
+  * qual funcao realizar por exemplo em inserir o i ja eh sufiente 
+  * para saber que se deseja inserir na lista
+  * i(nsert) d(elete) p(rint) e q(uit)
+  */
+  switch(str[0]){
+    case 'i':
+      adicionarNome(lista,nome);
+      break;
+
+    case 'd':
+      break;
+
+    case 'p':
+      printf("A lista esta:\n");
+      printaLista(lista->iniLista);
+      printf("\n\n");
+      break;
+
+    case 'q':
+      return 0;
+
+    default:
+      printf("Eh preciso digitar alguma instrucao que comece com i ,d ,p ou q\n");
+      printf("como inserir, deletar, printar ou quit");
+      break;
+  }
+
+  return 1; //qualquer opcao sem ser o q(uit) para sair
+}
+
+void printarMenuOpcoes(){
+  printf("Qual instrucao voce deseja realizar?\n\n");
+  printf("i nome- insere um nome na lista\n");
+  printf("d nome- deleta um nome da lista\n");
+  printf("p - mostra como esta a lista\n");
+  printf("q - sair\n\n");
+}
+
+/* Funcao main (principal)
+**/
 int main (void){
-  struct cabecaLista lista = iniciaLista();
-  char instrucao;
-  char info[TAM_INFO]; 
+  char instrucaoENome[TAM_INSTRUCAO_E_NOME];
+  int condicaoWhile;
+
+  Lista lista = iniciaLista();
+
   printf("Lista de nomes\n\n");
-  
-  do{
+  do {
+    printarMenuOpcoes();
 
-    printf("Qual instrucao voce deseja realizar?\n\n");
-    printf("i nome- insere um nome na lista\n");
-    printf("d nome- deleta um nome da lista\n");
-    printf("p - mostra como esta a lista\n");
-    printf("q - sair\n\n");
-
-    scanf("%c\n", &instrucao);
-
-    switch(instrucao){
-      case 'i':
-        scanf("%s", info);
-        adicionarNode(lista,info);
-        break;
-      case 'd':
-        break;
-      case 'p':
-        printaLista(lista.iniLista);
-        break;
-      case 'q':
-        break;
-      default:
-        break;
-    }
-    getchar();
-  }while(instrucao != 'q');
-  
-  printaLista(lista.iniLista);
+    //eh preciso somar um pois esta incluido o final da leitura '\0'
+    fgets(instrucaoENome,(TAM_INSTRUCAO_E_NOME + 1),stdin);  
+    printf("\n");
+    
+    condicaoWhile = tratarInstrucaoENome(instrucaoENome,lista);
+  }while(condicaoWhile);
 
   printf("Tchau!");
-  liberaLista(lista.iniLista);
+  liberaLista(lista);
   return 0;
 }
